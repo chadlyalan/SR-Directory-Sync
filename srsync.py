9cd43779d5d68a89f7bcf5e30bf14f08b9039d09
chadlyalan <chad__thomas@hotmail.com>
2017-10-25 11:35:08 -0600

    Modular functions have been included, now works with any directory named after the Tech Support Engineer's siebel username.

--- srsync.py
+++ srsync.py
@@ -9,129 +9,123 @@ import shutil
 #		will have their directories moved to a "Closed" 
 #		Directory.
 #
-# ************************************************************************
-# Ask for the username of the Service Request Owner to sync:
-#
-# And ask for the path to their gwsupport folder.
-# ************************************************************************
- 
-#user = raw_input("Username: ")
-
-# if (input("Is the script in your gwsupport directory? (y/n)") == 'y'):
-# 	path = os.getcwd()
-# else:
-# 	path = input("Please enter the full path to your gwsupport directory: ")
-
-#print 'Your name is ' + user
-# print(path)
-#
+#		Shane suggests using the request library so that I can pull the json info from 
+# 		the url without using the os library, apparently it can be really slick.
+#		also, I'd have to put the request library somewhere for the script to access it.
+#		  
 #	1. First, check if the open and closed dir's exists, if they don't: 
 # 				create the "open" directory
 #				create the "closed" directory
 #
-os.path.exists("open") or os.mkdir("open")
-os.path.exists("closed") or os.mkdir("closed")
+def maybeCreateFolder(string):
+	os.path.exists(string) or os.mkdir(string)
 #			
 #
 #		
-#		
-#	a line to put the SR info into a variable
-result = os.popen("curl http://proetus.provo.novell.com/qmon/brief-tse-json.asp?tse=cthomas").read()
-# *** Parse the string into a json object for reading
-json_srs = json.loads(result)
+#	
+def getNameOfDir():
+	# this splits the full path by /, and then returns the last item.
+	#return the string "name of the dir" or "username of engineer"
+	return os.getcwd().split('/')[-1]
 
-openSrs = {}
-for i in range(len(json_srs)):
-	openSrs[i] = json_srs[i]['SR']
+def getSrList(user):
+	#	a line to put the SR info into a variable
+	result = os.popen("curl http://proetus.provo.novell.com/qmon/brief-tse-json.asp?tse=%s" % user).read()
+	# *** Parse the string into a json object for reading
+	json_srs = json.loads(result)
+
+	openSrs = {}
+	for i in range(len(json_srs)):
+		openSrs[i] = json_srs[i]['SR']
+	return openSrs
 #			
 #		 
 #	Create a list of directories excluding closed and open
 # by assigning to the slice list[:] you can mutate the existing list to contain only the items you want
-allFolders = os.listdir(os.getcwd())
-allFolders[:] = [x for x in allFolders if not os.path.isfile(x) and not x == 'open' and not x == 'closed']
+def getFolderList():
+	allFolders = os.listdir(os.getcwd())
+	allFolders[:] = [x for x in allFolders if os.path.isdir(x) and not x == 'open' and not x == 'closed']
+	return allFolders
 
 # compare all folders against openSrs, when they match, send to open, else, send them to closed
 
-
-for x in allFolders:
-	for i in openSrs:
-		if x == openSrs[i]:
-			try:
-				print("Moving: " + x + " to open")
-				shutil.move(x, "open/")
-				break
-			except Exception:
-				print(x + " something went wrong, duplicate directory name probably, here's there error: ")
-				print(Exception)
-		else:
-			if i == len(openSrs) - 1:
+def organizeFolders(folders, srs):
+	for x in folders:
+		for i in srs:
+			if x == srs[i]:
 				try:
-					print("Moving: " + x + " to closed")
-					shutil.move(x, "closed/")
+					print("Moving: " + x + " to open")
+					shutil.move(x, "open/")
+					break
 				except Exception:
 					print(x + " something went wrong, duplicate directory name probably, here's there error: ")
 					print(Exception)
+			else:
+				if i == len(srs) - 1:
+					try:
+						print("Moving: " + x + " to closed")
+						shutil.move(x, "closed/")
+					except Exception:
+						print(x + " something went wrong, duplicate directory name probably, here's there error: ")
+						print(Exception)
 
-
-
-# this line moves everything other than closed, open, and srsync into open... might not use it.
-#subprocess.check_call(["bash", "-O", "extglob", "-c", "mv !(closed|open|srsync.py) open"])
-
-
-# *****************************************************************************************************
-#  clear up the allFolders variable as it doesn't serve any purpose anymore
-del allFolders
-#			
-#				
-#			os.chdir()  #move focus to a different dir
-# *********************************************************************************************************
-#  At this point we've sorted existing directories to closed, and open. Now create new dirs in open
-#  when an open sr doesn't already have a directory there.
-
-os.chdir(os.getcwd() + "/open")
-
-#
-# create a new list of folders in current directory
-openFolders = os.listdir(os.getcwd())
 
 
 # for loop to verify the openSr has a directory for it, if it doesn't create one.
-	for i in openSrs:
-		if not os.path.isdir(os.getcwd() + "/" + openSrs[i]):
-			print(openSrs[i] + " doesn't exist, let's make it")
-			os.mkdir(openSrs[i])
+def createOpenDir(openList):
+	for i in openList:
+		if not os.path.isdir(os.getcwd() + "/" + openList[i]):
+			print(openList[i] + " doesn't exist, let's make it")
+			os.mkdir(openList[i])
 
-
-# 	You can use list comprehension to create a new list containing only the elements you DON'T want to remove
-# somelist = [x for x in somelist if not determine(x)]
-
-# or by assigning to the slice list[:] you can mutate the existing list to contain only the items you want
-
-
-
-#
 #	4. step through all of the openFolders and move folders that 
 # 		aren't in the opensrs list anymore to closed.
 #	
 def moveToClosed(openList, currentList):
 	try:
-		move = False
 		for i in currentList:
 			for x in openList:
-				if i == x:
+				if i == openList[x]:
 					break
 				elif x == len(openList) - 1:
 					print("Moving: " + i + " to closed")
-					shutil.move(i, "closed/")
+					shutil.move(i, "../closed/")
 		
 		
 	except Exception:
-		print(Exception)
-# 		
-#		
-#		recursively remove empty directories with 'os.rmdirs()'
-#			os.removedirs('dir_a/dir_b/dir_c')
-#		
+		print("moveToClosed: ", Exception)
+
+
+# ****************************************************************
+# Main:
 #
+# This will control the sequence of events.
 #
-#		
+# ****************************************************************
+def main():
+	""" Main controller """
+
+	# open and closed folders?
+	maybeCreateFolder('open')
+	maybeCreateFolder('closed')
+
+	# get username or name of current directory, get list of SR's, get list of 
+	# folders in current directory, and sort them to open or closed
+	username = getNameOfDir()
+	organizeFolders(getFolderList(), getSrList(username)) 
+
+	# change to the open directory
+	os.chdir(os.getcwd() + "/open")
+
+	# create dirs for each open SR if it's not already there.
+	createOpenDir(getSrList(username))
+
+	# create a new list of folders in current directory (which is 'open' right now)
+	openFolders = os.listdir(os.getcwd())
+
+	# make sure to move closed SR folders out of open
+	moveToClosed(getSrList(username), openFolders)
+
+
+if __name__ == "__main__":
+	main()
